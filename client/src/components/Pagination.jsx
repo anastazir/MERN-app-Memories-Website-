fee84c4094ef7664aa0e6ae2afdc1c50ca6af102
Anas Tazir <atazir2018@gmail.com>
2021-07-07 19:27:04 +0530

    add basic fuctionality to Pagination.jsx and styles.js

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import {Pagination, PaginationItem} from '@material-ui/lab'
+import {Link} from 'react-router-dom'
 
+import styles from './styles'
 
 export const Pagination = () => {
+    const classes = useStyles
     return (
-        <div>
-            
-        </div>
+        <Pagination
+            className={{ul: classes.ul}}
+            count= {5}
+            page={1}
+            color='primary'
+            renderItem={(item) =>(
+                <PaginationItem {...item} component={Link} to={`/posts?page=${1}`}/>
+            )}
+        />
     )
 }
