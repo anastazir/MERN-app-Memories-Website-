c35624796b436ee563152d8280c182a0d77ee91d
Anas Tazir <atazir2018@gmail.com>
2021-09-03 10:56:10 +0530

    edit connection URL

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://admin123:AUGUST18@cluster0.f25pq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://YOUR_USERNAME:YOUR_PASSWORD@cluster0.f25pq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
